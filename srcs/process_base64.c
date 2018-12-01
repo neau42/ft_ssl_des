0b4baae670849b60c00a4e3f50d90e94c015bc68
no <no@42.fr>
2018-12-01 08:46:11 +0100

    base64 decode ~

--- srcs/process_base64.c
+++ srcs/process_base64.c
@@ -6,7 +6,7 @@
 /*   By: no <no@student.42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/19 12:56:19 by nboulaye          #+#    #+#             */
-/*   Updated: 2018/12/01 08:00:28 by no               ###   ########.fr       */
+/*   Updated: 2018/12/01 08:45:52 by no               ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,7 +35,7 @@ int		get_output_file(char *file_name)
 	{
 		fd = open(file_name, O_WRONLY | O_CREAT, 0644);
 		if (fd < 0)
-			ft_fdprintf(2, "ft_ssl: %s: No such file or directory\n", file_name);
+			ft_fdprintf(2, "ft_ssl: %s: can't be create\n", file_name);
 	}
 	else
 		fd = STDOUT_FILENO;
@@ -52,12 +52,12 @@ void	close_fds(t_base64 *base)
 
 void	print_n_init_static(t_base64 *base, uint8_t *val, int *i)
 {
-	if (*i)
-	{
+	if (*i >= 1)
 		ft_fdprintf(base->fd_o, "%c", val[0]);
+	if (*i >= 2)
 		ft_fdprintf(base->fd_o, "%c", val[1]);
+	if (*i >= 3)
 		ft_fdprintf(base->fd_o, "%c", val[2]);
-	}
 	*i = 0;
 	val[0] = 0;
 	val[1] = 0;
@@ -70,7 +70,10 @@ void 	get_b64_decode_value(t_base64 * base, uint8_t idx, int op)
 	static int		i = 0;
 
 	if (op)
+	{
+		i-=1;
 		print_n_init_static(base, val, &i);	
+	}
 	else if (i == 3)
 	{
 		val[2] += idx;
@@ -79,14 +82,12 @@ void 	get_b64_decode_value(t_base64 * base, uint8_t idx, int op)
 	else if (i == 2)
 	{
 		val[1] += ((idx & 0x3C) >> 2);
-		val[2] = ((idx & 0x3) << 6);
-		i++;
+		val[i++] = ((idx & 0x3) << 6);
 	}
 	else if (i == 1)
 	{
 		val[0] += ((idx & 0x30) >> 4);
-		val[1] = ((idx & 0xF) << 4);
-		i++;
+		val[i++] = ((idx & 0xF) << 4);
 	}
 	else if (i == 0)
 		val[i++] = (idx << 2);
@@ -104,31 +105,29 @@ int		get_one_chars(t_base64 *base, char c, char *tab)
 	else if (c == '=')
 	{
 		get_b64_decode_value(base, 0, 1);
+		ft_fdprintf(2, "11return   pour c : '%c'(%u)\n",c, c); 
 		return (1);
 	}
 	else if ((ptr = ft_strchr(tab, c)))
 		get_b64_decode_value(base, ptr - tab, 0);
 	else
+	{
+		ft_fdprintf(2, "22return   pour c : '%c'(%u)\n",c, c); 
 		return (1);
+	}
 	return (0);
 }
 
 void b64_decode(t_base64 *base, char *buf, int size, char *tab)//bufsize = 4
 {
-	// static uint8_t 	val[3] = {0};
-	// static int		i = 0;
 	int i = 0;
 
-	while (i < size)// && buf[i] != '=')
+	while (i < size)
 	{
-		// ft_printf("buf loop: size: %d\n", size);
 		if (get_one_chars(base, buf[i], tab))
 			return ;
-		// size--;
 		i++;
 	}
-	// if (buf && *buf && *buf != '=')
-		// b64_decode(base, buf, size, tab);
 }
 
 int		valid_chunk(char *buf, int *len, char *tab)
@@ -136,17 +135,13 @@ int		valid_chunk(char *buf, int *len, char *tab)
 	int i = -1;
 	char *ptr;
 
-	// while ((*len) && (buf[*len - 1] == '=' || ft_isspace(buf[*len - 1])))
-	// 	(*len)--;
 	while (++i < *len)
 	{
 		if (ft_isspace(buf[i]) || buf[i] == '=')
-		{
 			;
-		}
 		else if (!(ptr = ft_strchr(tab, buf[i])))
 		{
-			ft_printf("not valide: '%c'\n", buf[i]);
+			ft_fdprintf(2, "not valide: '%c'\n", buf[i]);
 			return (0);
 		}
 	}
